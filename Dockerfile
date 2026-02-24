b560469511a991d19a4489e813065e5b4c6a6ca0
cesarmata1 <ccesarmata@gmail.com>
2026-02-24 13:53:16 +0100

    primer commit tp3 vue

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Etapa 1: Build
+FROM node:20-alpine AS build-stage
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+# Etapa 2: Producción con Nginx
+FROM nginx:alpine AS production-stage
+COPY --from=build-stage /app/dist /usr/share/nginx/html
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
+
+
+
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>TP3 - Déploiement Vue.js avec Docker 🚀</h1>
+    <h2>Application déployée sur Azure App Service</h2>
+    <p>Conteneurisé avec Docker</p>
+    <p>Image hébergée sur Docker Hub</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+.container {
+  text-align: center;
+  margin-top: 100px;
+  font-family: Arial, sans-serif;
+}
+h1 {
+  color: #42b883;
+}
+</style>
